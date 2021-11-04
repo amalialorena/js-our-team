cec19678952a982334dddc49959bb47ddd89ccb5
Amalia <amalialorenaferariu@gmail.com>
2021-11-04 15:23:55 +0100

    cicli for

--- js/script.js
+++ js/script.js
@@ -8,31 +8,31 @@ const team =
         'nome':'Wayne Barnett',
         'ruolo':'Founder & CEO',
         'foto': 'img/wayne-barnett-founder-ceo.jpg'
-    }
+    },
 
     {
         'nome':'Angela Caroll',
         'ruolo':'Chief editor' ,
         'foto':'img/angela-caroll-chief-editor.jpg'
-    }
+    },
 
     {
         'nome':'Walter Gordon',
         'ruolo':'Office manager',
         'foto':'walter-gordon-office-manager.jpg'
-    }
+    },
 
     {
         'nome':'Barbara Ramos',
         'ruolo':'Graphic designer',
         'foto':'img/barbara-ramos-graphic-designer.jpg'
-    }
+    },
 
     {
         'nome':'Angela Lopez',
         'ruolo':'Media manager',
         'foto':'img/angela-lopez-social-media-manager.jpg'
-    }
+    },
 
     {
         'nome':'Scott Estrada',
@@ -44,7 +44,29 @@ const team =
 ]
 
 // - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team. 
-     //faccio un ciclo che mi prende tutti i dati contenuti nei oggetti
+     //faccio un ciclo che mi prende tutti i dati contenuti negli oggetti
        // quei dati li stampo in pagina 
+
+let cardsContainer = document.querySelector(".team-container");
+
+for(let i = 0; i < team.length; i++) {
+    let teamObj = team[i]
+    for (let key in teamObj) {
+        console.log(teamObj[key]);
+    }
+}
+
 // - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
 
+cardsContainer.innerHTML += `<div class="team-card">
+<div class="card-image">
+  <img
+    src="img/wayne-barnett-founder-ceo.jpg"
+    alt="Wayne Barnett"
+  />
+</div>
+<div class="card-text">
+  <h3>Wayne Barnett</h3>
+  <p>Founder & CEO</p>
+</div>
+</div>`
